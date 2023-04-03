53e71b161032537ec6240dda037994b9a2694c82
npetraco <npetraco@gmail.com>
2023-04-03 16:44:33 -0400

    Remote update

--- R/file_utils.R
+++ R/file_utils.R
@@ -79,10 +79,11 @@ op.model.file <- open_mf
 open_model_file <- open_mf
 
 
-#' Write a JAGS or Stan model to file
+#' Write a JAGS or Stan model to file to the tempdir
 #'
-#' Write a JAGS or Stan model to a temp file. Model should be formatted as a string. Intended for
-#' use with JAGS since rstan can already deal with model strings but JAGS needs a stand alone file.
+#' Write a JAGS or Stan model to a temp file. Model should be formatted as a string
+#' in an R script. This is really intended for use with JAGS since rstan can already
+#' deal with model strings but JAGS needs a stand alone file.
 #'
 #' @param XX XX
 #' @return The function will XX
@@ -101,18 +102,24 @@ write.tmp.model.file <- function(model.string) {
 }
 # Function aliases for write.tmp.model.file
 #' @export
-write.model.file <- write.tmp.model.file
+write.model.file      <- write.tmp.model.file
 #' @export
-write.mf         <- write.tmp.model.file
+write.mf              <- write.tmp.model.file
 #' @export
-write_mf         <- write.tmp.model.file
+write_mf              <- write.tmp.model.file
 #' @export
-write_model_file <- write.tmp.model.file
+write_model_file      <- write.tmp.model.file
+#' @export
+write_temp_model_file <- write.tmp.model.file
 
 
-#' Read in specified JAGS or Stan model file contained in bayesutils as a string
+
+#' Read in specified JAGS or Stan model file contained in bayesutils or at the end of a specified path as a string
 #'
-#' Read in specified JAGS or Stan model file contained in bayesutils as a string
+#' Read in specified JAGS or Stan model file contained in bayesutils or at the end of a
+#' specified path string. This is useful (and keeps code bloat down) when writing your
+#' own JAGS and Stan files that you want to load in and use. If no model path is specified
+#' the function looks for the model file in the bayesutils library.
 #'
 #' @param XX XX
 #' @return The function will XX
@@ -137,3 +144,67 @@ read.mf         <- read.model.file
 read_mf         <- read.model.file
 #' @export
 read_model_file <- read.model.file
+
+
+#' Download and load a script file
+#'
+#' Download and load a script file. Intended to get script files for the Notes
+#'
+#' @param XX XX
+#' @return The function will XX
+#'
+#'
+#' @export
+get.script <- function(script.name, url_head=NULL, download_location=NULL) {
+
+  if(is.null(url_head)) {
+    url_head.loc <- "https://raw.githubusercontent.com/npetraco/MATFOS705/master/Notes_scripts_bank"
+  } else {
+    url_head.loc <- url_head
+  }
+
+  if(is.null(download_location)) {
+    download_location.loc <- tempdir()
+  } else {
+    download_location.loc <- download_location
+  }
+
+  url_loc <- paste0(url_head.loc, "/", script.name)
+  #print(url_loc)
+
+  file_loc <- paste0(download_location.loc,"/",script.name)
+  #print(file_loc)
+
+  download.file(url_loc, file_loc)
+  print("File downloaded to:")
+  print(file_loc)
+  file.edit(file_loc)
+
+}
+
+
+#' Get a model file or download and load a script file
+#'
+#' Get a model file or download and load a script file. Combines functionality of
+#' get.script() and open_mf() into one function for easier use.
+#'
+#' @param XX XX
+#' @return The function will XX
+#'
+#'
+#' @export
+get_file <- function(file.name, url_head=NULL, download_location=NULL){
+
+  # First determine if file.name is a JAGS, Stan or R-script file:
+  nme.vec <- strsplit(file.name, split = ".", fixed = T)[[1]]
+  if(("bug" %in% nme.vec) | ("stan" %in% nme.vec)) { # Is it a model file?
+
+    open_mf(file.name)
+
+  } else { # Otherwise assume its a script file
+
+    get.script(file.name, url_head, download_location)
+
+  }
+
+}
--- tests/model.file.open.test.R
+++ tests/model.file.open.test.R
@@ -1,4 +1,5 @@
-?View
+library(bayesutils)
+
 file.edit(system.file("jags/binomial_logitnormal.bug.R", package = "bayesutils"))
 
 file.edit(system.file("stan/binomial_logitnormal.stan", package = "bayesutils"))
@@ -18,3 +19,49 @@ op.mf("binomial_logitnormal.stan")
 get_model_file_path("binomial_logitnormal.bug")
 
 read.mf(model.file.name = "normal-T.stan", model.path = "/Users/karen2/Desktop/")
+
+get.script("binomial-logitnormal_JAGS.R")
+get.script("ex2_norm-T_control_STAN.R")
+open_model_file("norm-T.stan")
+
+
+get.script("ex1_prior_plots.R")
+op.mf("exp-gamma.bug")
+op.mf("exp-gamma.stan")
+get.script("ex1_exp-gamma_control_JAGS.R")
+get.script("ex1_exp-gamma_control_STAN.R")
+op.mf("exp-uniform.bug")
+op.mf("exp-uniform.stan")
+op.mf("exp-trunc-T.bug")
+op.mf("exp-trunc-T.stan")
+
+get_file("ex2_prior_plots.R")
+get_file("norm-T.bug")
+get_file("norm-T.stan")
+
+get_file("ex2_norm-T_control_JAGS.R")
+get_file("ex2_norm-T_control_STAN.R")
+
+get_file("binomial-uniform.bug")
+get_file("binomial-uniform.stan")
+
+get_file("ex3_binomial-uniform_control_JAGS.R")
+get_file("ex3_binomial-uniform_control_STAN.R")
+get_file("binomial-uniform_multiple.bug")
+get_file("binomial-uniform_multiple.stan")
+
+get_file("ex3_binomial-uniform_multiple_control_JAGS.R")
+get_file("ex3_binomial-uniform_multiple_control_STAN.R")
+
+get_file("poisson-gamma.bug")
+get_file("poisson-gamma.stan")
+get_file("ex4a_poisson-gamma_control_JAGS.R")
+get_file("ex4a_poisson-gamma_control_STAN.R")
+
+get_file("poisson-gamma_multiple.bug")
+get_file("poisson-gamma_multiple.stan")
+get_file("ex4b_poisson-gamma_control_JAGS.R")
+get_file("ex4b_poisson-gamma_control_STAN.R")
+
+get_file("ex4c_poisson-gamma_control_JAGS.R")
+get_file("ex4c_poisson-gamma_control_STAN.R")
